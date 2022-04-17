3bdf14157461912e024c8d8da9801cdc4eb75e8b
mubariz <syedmubariz542@gmail.com>
2022-04-18 00:27:21 +0530

    crud operation working

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require('express')
-const defaultCtrl = require('./constrollers/defaultCtrl')
-
+const defaultCtrl = require('./controllers/defaultCtrl')
+const BikeCtrl = require('./controllers/bikesctrl')
+const carsCtrl = require('./controllers/carsCtrl')
 const app = express()
+const bodyPasrser = require('body-parser')
 
 app.listen(3000,function ()
     {console.log('server is running on port 3000')
 })
 
+app.use(bodyPasrser.json())
 
-app.get('/bike',function (req,res){
-    const bike =[ {
-                   id : ' 02',
-                    name:'yamaha'
-               },{
-                id : ' 02',
-                name:'apache'
-               },
-               {
-                id : ' 03',
-                name:'pulsur'
-               }
-            ]
-    res.json(bike)
-    res.status(204)
-})
+
+app.get('/bike',BikeCtrl.get)
+app.get('/cars',carsCtrl.get)
+app.post('/cars',carsCtrl.create)
+app.delete('/cars/:id',carsCtrl.delete)
+app.put('/cars/:id',carsCtrl.update)
+app.patch('/cars/:id',carsCtrl.patch)
+app.get('/cars/:id',carsCtrl.getById)
 app.get('/',defaultCtrl.home)
 app.get('/health',defaultCtrl.health)
