05bed5b444356b8a99981c063ca5aca979e90ecf
mubariz <syedmubariz542@gmail.com>
2022-04-18 18:22:34 +0530

    data base
    mongo db health end point is connected

--- controllers/defaultCtrl.js
+++ controllers/defaultCtrl.js
@@ -1,11 +1,30 @@
+const mongoose = require('mongoose')
+const dataBase = require('../config/index')
 function get(req,res){
     res.json("Node api")
     res.status(201)
 }
 
-function health(req,res){
-    res.json({health:"up"})
-    res.status(201)
+async function health(req,res){
+    try{
+        await mongoose.connect(dataBase.dbConStr)
+        res.json({db:"up"})
+        res.status(201)
+    }catch(e){
+        res.send(e,'internal server error')
+        res.status(500)
+    }
+   
+   
+   
+    // .then(()=>{
+    //     res.json({db:"up"})
+    //     res.status(201)
+    // })
+    // .catch(()=>{
+    //     res.status(500);
+    //     res.send('internal servedr error')
+    // })
 }
 
 module.exports = {get,health}
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const defaultRoutes = require('./Routers/defaultRoutes')
+const dataBase = require('./config/index')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
-
 const port = 3000;
 
 app.listen(port,()=>{console.log(`server is running on ${port}`)})
+
+
+mongoose.connect(dataBase.dbConStr)
+.then(()=>{console.log('mongo db is connected')})
+.catch(()=>{console.log('failed to connect to database')})
+
+
+app.use(bodyParser.json())
 app.use('/',defaultRoutes)
 
