f396c2e670024eb5c32f01a63e6a36608a0cc09a
mubariz <syedmubariz542@gmail.com>
2022-04-21 22:13:16 +0530

    request logs
    request logs can be accessed by library "morgan"

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
+
 const defaultRoutes = require('./Routers/defaultRoutes')
 const userRoute = require('./Routers/userRoutes')
 const dataBase = require('./config/index')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+
+
+
 const app = express()
 const port = 3000;
+
+fs.mkdirSync(path.join(__dirname,"logs")) 
+
+var LogStream = fs.createWriteStream(path.join(__dirname, 'logs','request.logs'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: LogStream }))
+
 
 app.listen(port,()=>{console.log(`server is running on ${port}`)})
 
