04ae4dcf8fe12ade4fdcc0052ec4423e21133181
mubariz <syedmubariz542@gmail.com>
2022-04-21 21:22:19 +0530

    role refactor

--- middleWare/authorize.js
+++ middleWare/authorize.js
@@ -1,12 +1,18 @@
+const role = {
+    candidate:0,
+    recruiter:1,
+    admin:2
+}
+
 const authorizeRecruiter = (req,res,next)=>{
-    if(req.role === 1){
+    if(req.role === role.recruiter){
         next()
     }else{
         res.status(403).send("forbidden request")
     }
 }
 const authorizeAdmin = (req,res,next)=>{
-    if(req.role === 2) next()
+    if(req.role === role.admin) next()
     else res.status(403).send('forbidden ')
 }
 
